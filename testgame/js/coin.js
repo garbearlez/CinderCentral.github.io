5f8af1a065db656a4392cc67d4339c600d20bf9d
Garbearlez <garbearlez@gmail.com>
2017-09-05 10:27:55 -0400

    Delete coin.js

--- testgame/js/coin.js
+++
@@ -1,6 +0,0 @@
-// create a reference to our element
-var element = document.querySelector('#coin');
-// create our Motio sprite
-var sprite = new Motio(element, {fps: 12, frames: 19});
-// start animation
-sprite.play();
