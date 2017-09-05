b7683575303ce5776d1f67952e3931b52ab07556
Garbearlez <garbearlez@gmail.com>
2017-09-05 08:00:30 -0400

    Delete character.js

--- game/character.js
+++
@@ -1,6 +0,0 @@
-// create a reference to our element
-var element = document.querySelector('#char');
-// create our Motio sprite
-var sprite = new Motio(element, {fps: 2, frames: 2});
-// start animation
-sprite.play();
