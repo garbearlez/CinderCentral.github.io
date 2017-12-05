83909ef8d066d21b213de2b9f3bdfd45e3e063fc
Darkoaklog <andrewporetsky@gmail.com>
2017-12-05 11:08:42 -0500

    Update scripts.js

--- chat/scripts.js
+++ chat/scripts.js
@@ -33,7 +33,7 @@ $('.chat').on('submit',function(e) {
   // create a message object
   var message = {
     name : nameField.val(),
-    text : messageField.val() + " - " +  date.getHours() + ":" + date.getMinutes() + " " + date.getDate() + "-" + date.getMonth()
+    text : messageField.val() + " - " +  date.getHours() + ":" + date.getMinutes() + " " + date.getDate() + "/" + date.getMonth()
   }
 
   // Save Data to firebase
